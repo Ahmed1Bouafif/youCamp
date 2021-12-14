104878fcd4b39c936493d2077006caece74d7ac7
jouinibilel <bileljouinipro@hotmail.com>
2021-12-14 20:09:30 +0100

    starter pack

---
+++ server/db/index.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const database = 'youcamp';
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: database
+
+});
+
+
+
+
+
+  module.exports = connection;
